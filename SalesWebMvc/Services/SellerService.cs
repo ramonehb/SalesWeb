58ea748f7aaa3ea355b9553b1fb61a14bf27d410
Humberto <humberto.ramone@csprime.com.br>
2022-06-30 12:45:59 -0300

    Departament Foreign Key

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -22,6 +22,7 @@ namespace SalesWebMvc.Services
 
         public void Insert(Seller seller)
         {
+            seller.Department = _context.Department.First();
             _context.Add(seller);
             _context.SaveChanges();
         }
